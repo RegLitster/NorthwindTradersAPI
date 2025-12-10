37eb221de19d45b3234e8f209155e502592496c3
Reggie <reglitster00@gmail.com>
2025-12-10 11:23:38 -0500

    Added exercise 1

--- src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @RequestMapping(path="/", method = RequestMethod.GET)
-    public String home(@RequestParam(defaultValue = "Test") String test) {
-        return test;
+    public String home(@RequestParam(defaultValue = "country", required = false) String country) {
+
+        if(!country.equals("country")) {
+            return "Hello " + country;
+        }
+        return "Hello World";
 
     }
 }
